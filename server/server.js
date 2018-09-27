7587a97c0b63b23a4b4c449dd08c069616f12682
mujd <mujd19@gmail.com>
2018-09-27 04:10:21 -0300

    Login normal seccion 10

--- server/server.js
+++ server/server.js
@@ -4,14 +4,20 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+// parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }));
+
+// parse application/json
 app.use(express.json());
-app.use(require('./routes/usuario'));
+
+// Configuración global de rutas
+app.use(require('./routes/index'));
 
 const options = {
     useNewUrlParser: true,
     useCreateIndex: true
 };
+
 mongoose.connect(process.env.URLDB, (err, res) => {
     if (err) throw err;
 
