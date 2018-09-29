88bd0c7204bb8f5112264f3eeda5ad71f594846f
mujd <mujd19@gmail.com>
2018-09-29 00:01:31 -0300

    Productos y categorias

---
+++ server/routes/categoria.js
@@ -0,0 +1,164 @@
+const express = require('express');
+let { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
+
+let app = express();
+
+let Categoria = require('../models/categoria');
+
+// ============================
+// Mostrar todas las categorias
+// ============================
+app.get('/categoria', verificaToken, (req, res) => {
+    Categoria.find({}, 'descripcion usuario')
+        .sort('descripcion')
+        .populate('usuario', 'nombre email')
+        .exec((err, categorias) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            Categoria.count({}, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    categorias,
+                    total: conteo
+                });
+            });
+
+        });
+});
+
+// ============================
+// Mostrar una categoria por ID
+// ============================
+app.get('/categoria/:id', verificaToken, (req, res) => {
+    //Categoria.findById();
+    let id = req.params.id;
+    Categoria.findById(id, (err, categoriaDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if (!categoriaDB) {
+            return res.status(500).json({
+                ok: false,
+                err: {
+                    message: 'El ID no es correcto'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+    });
+});
+
+// ============================
+// Crear nueva categoria
+// ============================
+app.post('/categoria', verificaToken, (req, res) => {
+    // regresa la nueva categoria
+    // req.usuario._id
+    let body = req.body; //Obtener el body
+
+    let categoria = new Categoria({
+        descripcion: body.descripcion,
+        usuario: req.usuario._id // Con middleware verificaToken se puede obtener el id del usuario
+    });
+
+
+    categoria.save((err, categoriaDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+    });
+});
+
+// ============================
+// Actualizar categoria
+// ============================
+app.put('/categoria/:id', (req, res) => {
+    let id = req.params.id;
+    let body = req.body;
+
+    let descCategoria = {
+        descripcion: body.descripcion
+    }
+
+    Categoria.findByIdAndUpdate(id, descCategoria, { new: true, runValidators: true }, (err, categoriaDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+    });
+});
+
+// ============================
+// Borrar categorias
+// ============================
+app.delete('/categoria/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
+    // solo un administrador puede borrar categorias
+    // Categoria.findByIdAndRemove
+    let id = req.params.id;
+
+    Categoria.findByIdAndRemove(id, (err, categoriaBorrada) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if (!categoriaBorrada) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Categoria no encontrada, el id no existe'
+                }
+            });
+        }
+        res.json({
+            ok: true,
+            /* categoria: categoriaBorrada, */
+            message: 'Categoria Borrada'
+        });
+    });
+});
+
+module.exports = app;
